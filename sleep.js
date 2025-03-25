29ff4605445a64b85eeefc99fb91da2ff789bd14
AakashJethani <aakashjethani2000@gmail.com>
2025-03-26 00:18:23 +0530

    25March2025

---
+++ sleep.js
@@ -0,0 +1,17 @@
+// Explanation
+// Explanation:
+
+// The sleep function takes a positive integer millis as input.
+// It then creates a Promise that resolves after millis milliseconds using the setTimeout function.
+// The Promise is then awaited using the await keyword, which allows the function to pause execution until the Promise is resolved.
+// If you are not familar with 'await' then you may use 'return' instead like this:
+// return new Promise(resolve => setTimeout(resolve, millis));
+
+async function sleep(millis) {
+    let prom = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(millis)
+        },millis)
+    })
+    return prom
+}
